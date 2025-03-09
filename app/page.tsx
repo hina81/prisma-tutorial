11ebd9cdc2abcb850bc3313aa54dccd67c2ce360
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-03-09 22:57:47 +0900

    prismaclient

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ItemCardList from "@/components/ItemCardList";
 import MenuBar from "@/components/Menu-bar";
+import { prisma } from "./lib/prisma";
 
 export default function Home() {
   return (
