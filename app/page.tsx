30ae2a730d565cf770f83cc9ba81eaa9f9e431f1
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-03-10 01:01:49 +0900

    ISRとitems取得

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import ItemCardList from "@/components/ItemCardList";
 import MenuBar from "@/components/Menu-bar";
-import next from "next";
+import { itemData } from "./types/types";
 
-
-export default async function Home() {
+async function getAllItems() {
   const response = await fetch("http://localhost:3000/api/items", {
     next: { revalidate: 300 },
   });
 
-  const allItems = await response.json();
-  console.log(allItems)
+  const allItems: itemData[] = await response.json();
+  return allItems;
+}
+
+export default async function Home() {
+  const allItems = await getAllItems();
   return (
     <div className="m-10">
       <MenuBar />
-      <ItemCardList />
+      <ItemCardList allItems={allItems} />
     </div>
   );
 }
